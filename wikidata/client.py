7871b24bdbe0ca66f1a3072cb48690778a803989
Alexa <5918060+alexabruck@users.noreply.github.com>
2021-03-12 11:45:34 +0100

    Specify OPTV as user agent in wikidata queries

--- wikidata/client.py
+++ wikidata/client.py
@@ -7,7 +7,7 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 ENDPOINT_URL = "https://query.wikidata.org/sparql"
 
 def get(query):
-    user_agent = "WDQS-example Python/%s.%s" % (sys.version_info[0], sys.version_info[1])
+    user_agent = "OpenParliamentTV/0.1 (https://openparliament.tv; joscha.jaeger@openparliament.tv)"
     # TODO adjust user agent; see https://w.wiki/CX6
     sparql = SPARQLWrapper(ENDPOINT_URL, agent=user_agent)
     sparql.setQuery(query)
