f47c596102c6fd2801222b5db50518c5668fcf5f
Alexa <5918060+alexabruck@users.noreply.github.com>
2021-04-23 15:41:44 +0200

    Add offset parameter for batch dumping

--- dump_batched.py
+++ dump_batched.py
@@ -1,6 +1,13 @@
+import sys
 import json
 import wikidata.client as wikidata_client
 from wikidata.queries import get_all_members_of_parliament
+
+BATCH_OFFSET = 0
+try:
+    BATCH_OFFSET = int(sys.argv[1])
+except:
+    pass
 
 batches = [
     {'max_birth': '1900-01-01'},
@@ -16,7 +23,7 @@ batches = [
     {'min_birth': '1980-01-01'},
 ]
 
-for batch in batches:
+for batch in batches[BATCH_OFFSET:]:
     query = get_all_members_of_parliament(parliament="DE", **batch)
     results = wikidata_client.get(query)
     file = 'mdb_' + batch.get('min_birth', 'before') + '__' + batch.get('max_birth', 'after') + '.json'
