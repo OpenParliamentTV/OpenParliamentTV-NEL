0928a759844b6cfe27047d5e29aca61fc05ad2de
Alexa <5918060+alexabruck@users.noreply.github.com>
2021-07-01 14:20:36 +0200

    Change output file path for scripts

--- db_dump/queries/factions.py
+++ db_dump/queries/factions.py
@@ -3,6 +3,8 @@ currentdir = os.path.dirname(os.path.realpath(__file__))
 rootdir = os.path.dirname(os.path.dirname(currentdir))
 sys.path.append(rootdir)
 
+OUTFILE = 'db_dump/data/factions/factions-rawqueryresults.json'
+
 import json
 import wikidata.client as wikidata_client
 from wikidata.queries import get_all_factions_of_germany
@@ -10,5 +12,5 @@ from wikidata.queries import get_all_factions_of_germany
 query = get_all_factions_of_germany()
 results = wikidata_client.get(query)
 print(results)
-with open('factions.json', 'w') as outfile:
+with open(OUTFILE, 'w') as outfile:
     json.dump(results, outfile)
