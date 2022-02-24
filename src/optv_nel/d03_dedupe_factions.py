b3d475f15a05ce88035f77cbe993aff9585e2af6
Alexa <5918060+alexabruck@users.noreply.github.com>
2022-02-24 15:30:05 +0100

    Just copy files to next step if no processing is needed

--- src/optv_nel/d03_dedupe_factions.py
+++ src/optv_nel/d03_dedupe_factions.py
@@ -1,1 +1,9 @@
-# Currently not necessary
+# No deduping necessary, just copying over the file from previous step
+import shutil
+from pathlib import Path
+
+SRC_PATH = "data/02_formatted/factions/factions.json"
+DEST_FILENAME = "factions.json"
+DEST_PATH = Path("data/03_deduped/factions")
+DEST_PATH.mkdir(parents=True, exist_ok=True)
+shutil.copy(SRC_PATH, DEST_PATH / DEST_FILENAME )
--- src/optv_nel/d05_enhance_parties.py
+++ src/optv_nel/d05_enhance_parties.py
@@ -1,1 +1,9 @@
-# Currently not necessary
+# No enhancing necessary, just copying over the file from previous step
+import shutil
+from pathlib import Path
+
+SRC_PATH = "data/03_deduped/parties/parties.json"
+DEST_FILENAME = "parties.json"
+DEST_PATH = Path("data/05_enhanced/parties")
+DEST_PATH.mkdir(parents=True, exist_ok=True)
+shutil.copy(SRC_PATH, DEST_PATH / DEST_FILENAME )
